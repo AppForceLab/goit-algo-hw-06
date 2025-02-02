5a9493f095938f594291e4eb813c304b12ee8811
Volodymyr Popov <smartappsvp@gmail.com>
2025-02-02 21:55:07 +0000

    fix

--- task3_dijkstra.py
+++ task3_dijkstra.py
@@ -1,3 +1,4 @@
+import heapq
 import networkx as nx
 import pandas as pd
 from task1_graph_analysis import G
@@ -5,7 +6,23 @@ from task1_graph_analysis import G
 # Завдання 3: Алгоритм Дейкстри
 
 def dijkstra(graph, start):
-    return nx.single_source_dijkstra_path_length(graph, start)
+    shortest_paths = {node: float('inf') for node in graph.nodes}
+    shortest_paths[start] = 0
+    priority_queue = [(0, start)]
+    
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+        
+        if current_distance > shortest_paths[current_node]:
+            continue
+        
+        for neighbor, attributes in graph[current_node].items():
+            distance = current_distance + attributes['weight']
+            if distance < shortest_paths[neighbor]:
+                shortest_paths[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    return shortest_paths
 
 shortest_paths = {node: dijkstra(G, node) for node in G.nodes}
 
